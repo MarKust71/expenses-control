b087438dd983e4b8ee80bc02c0f1d135b1e0fc0a
Marek Kustosz <markust71@hotmail.com>
2021-04-03 19:59:28 +0200

    add apollo-client

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,6 +1,33 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
+import { ApolloClient, ApolloProvider, from, HttpLink, InMemoryCache } from '@apollo/client';
+import { ErrorResponse, onError } from '@apollo/client/link/error';
+
+import { Cats } from './cats/Cats';
+
+const errorLink = onError(({ graphQLErrors, networkError }: ErrorResponse) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message, locations, path }) => {
+      alert(`Graphql error: ${message}`);
+    });
+  }
+});
+const link = from([
+  errorLink,
+  new HttpLink({
+    uri: 'http://localhost:8080/graphql',
+  }),
+]);
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link,
+});
 
 export const App = () => {
-  return <Typography>Learn React</Typography>;
+  return (
+    <ApolloProvider client={client}>
+      <Typography>Learn React</Typography>
+      <Cats />
+    </ApolloProvider>
+  );
 };
---
+++ client/src/app/cats/Cats.types.ts
@@ -0,0 +1,3 @@
+export type CatsProps = {
+  placebo?: string;
+};
