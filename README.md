cd49b90b29cff508856076c6f86623cb9cfbe172
Marek Kustosz <markust71@hotmail.com>
2021-04-03 16:19:47 +0200

    add mongodb

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # expenses-control
+
+Client: http://localhost:3000
+\
+Server: http://localhost:8080
+
+GraphQL Playground: http://localhost:8080/graphql
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,14 +3,18 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     CatsModule,
     GraphQLModule.forRoot({
       // autoSchemaFile: join(process.cwd(), 'schema/schema.gql'),
       autoSchemaFile: 'schema/schema.gql',
     }),
+    MongooseModule.forRoot(process.env.DB_CONNECTION_STRING),
   ],
   controllers: [AppController],
   providers: [AppService],
